d797cd6939c437dfc4a28a8802dc72de19c61fa8
m47251608-creator <m47251608@gmail.com>
2026-06-25 14:51:05 +0500

    Add configuration for bot settings
    2nd done

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: 'ASARTASH-MD',
+    ownerName: 'Asad-TechX',
+    ownerNumber: '923424267980',
+    prefix: '.',
+    sessionName: 'asartash_session',
+    poweredBy: '@Asad-TechX 🚩'
+}
